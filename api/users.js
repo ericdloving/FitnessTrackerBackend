4a357c3680fc6e4e24597cc552aa7d29b48edc9c
Thomas <thomas-g-smith@hotmail.com>
2022-07-27 12:24:09 -0500

    debugging

--- api/users.js
+++ api/users.js
@@ -1,9 +1,46 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
+const { getUserByUsername, createUser } = require("../db");
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = process.env;
 
 // POST /api/users/register
+router.post('/register', async (req,res,next)=>{
+    console.log(req, "we live in the forest")
+    try{
 
+        const {username,password} = req.body;
+        if (password.length < 8){
+            next({name: "passwordLengthError",
+            message: "The password is not long enough"})
+        }
+        const _user = await getUserByUsername(username)
+        if (_user){
+            next({name: "userExistsError",
+            message: "Username already registered"}
+            )
+        }
+
+        const user = await createUser({username,password})
+
+        const token = jwt.sign(
+            {
+              id: user.id,
+              username,
+            },
+            process.env.JWT_SECRET,
+          );
+
+          res.send({
+            message: "thank you for signing up",
+            token,
+          });
+    }catch ({name,message}){
+        next({name,message})
+    }
+})
+const {username,password} = 
 // POST /api/users/login
 
 // GET /api/users/me
