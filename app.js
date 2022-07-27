c93e88898cbef3ae78d377948ac58de883824303
Thomas <thomas-g-smith@hotmail.com>
2022-07-27 12:02:53 -0500

    set up server

--- app.js
+++ app.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const server = express();
+const app = express();
 
 
 const cors = require('cors');
-server.use(cors());
+app.use(cors());
 
 const morgan = require('morgan');
-server.use(morgan('dev'));
-const { PORT } = process.env;
+app.use(morgan('dev'));
 
-server.use('/api', require('./api'));
+app.use('/api', require('./api'));
 // Setup your Middleware and API Router here
-server.listen(PORT, () => {
-    console.log('The server is up on port', PORT)
-  });
 
-module.exports = server;
+
+module.exports = app;
